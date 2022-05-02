87b4f83a4828d741e4e6dcc9837914bd23000bff
StanislavProtashchuk <stasprotaskhyk@gmail.com>
2022-05-02 15:09:40 +0300

    stat update

--- src/components/Statistics/statistics.jsx
+++ src/components/Statistics/statistics.jsx
@@ -4,7 +4,7 @@ import s from './Statistics.module.css'
 export default function Statistics({ title, stats }) {
     return (
         <section className={s.statistics}>
-            <h2 className={s.title}>{title}</h2>
+            {title && <h2 className={s.title}>{title}</h2>}
 
             <ul className={s.statList}>
                 {stats.map(({ id, label, percentage }) => {
